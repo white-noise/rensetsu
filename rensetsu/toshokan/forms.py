7e5f62b72d4556c6f91c15ab1b106f2ae53ba393
Zane Marius Rossi <zmr@uchicago.edu>
2019-06-20 00:11:47 -0500

    removed form reference because unecessary.

--- rensetsu/toshokan/forms.py
+++ rensetsu/toshokan/forms.py
@@ -1,7 +1,7 @@
-from django import forms
+# from django import forms
 
-from base.models import UserProfile
+# from base.models import UserProfile
 
-class AddUserForm(forms.Form):
-    user = forms.ModelChoiceField(queryset=UserProfile.objects.all(),
-    	empty_label="Select")
+# class AddUserForm(forms.Form):
+#     user = forms.ModelChoiceField(queryset=UserProfile.objects.all(),
+#     	empty_label="Select")
--- rensetsu/toshokan/views.py
+++ rensetsu/toshokan/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-from .forms import AddUserForm
 from .models import Kanji
 
 @login_required
@@ -19,15 +18,12 @@ def individual(request, kanji_id):
     return render(request, 'toshokan/individual.html',
      {'kanji': kanji,
      'reading_eng': reading_eng,
-     'reading_jpn': reading_jpn,
-     'form': AddUserForm()
+     'reading_jpn': reading_jpn
      })
 
 @login_required
 def add_user(request, kanji_id):
     kanji = get_object_or_404(Kanji, pk=kanji_id)
     if request.method == 'POST':
-        form = AddUserForm(request.POST)
-        if form.is_valid():
-            kanji.interesting_kanji.add(form.cleaned_data['user'])
+        kanji.interesting_kanji.add(request.user.profile)
     return redirect('individual', kanji_id)
